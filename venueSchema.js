2b0b89b9f9a1e79fba76b7051152e96e40eab4e7
alviancode <alvianwijaya200@gmail.com>
2021-05-04 20:59:41 +0700

    first

--- venueSchema.js
+++
@@ -1,9 +0,0 @@
-/*const mongoose = require('mongoose')
-const { ObjectId } = mongoose.Schema.Types
-const venieSchema = new mongoose.Schema({
-    venue: String,
-    address: String
-})
-
-mongoose.model("Post", postSchema)
-*/
